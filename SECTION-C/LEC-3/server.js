40b50df5c055bce27614fe240f7df695a958d3e8
shiva-verma <jztshiva@gmail.com>
2025-01-30 21:15:35 +0530

    fix

--- SECTION-C/LEC-3/server.js
+++ SECTION-C/LEC-3/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
+const isLoggedIn = require('./isLoggedIn.js')
 
-app.get('/product/:id([0-9])/:price', (req, res)=>{
-    const {id, price} = req.params;
-    console.log(id)
-    console.log(price)
-    res.send('hello');
+function cb1(req, res, next){
+    next()
+}
+function cb2(req, res, next){
+    
+}
+
+
+app.get('/profile',[isLoggedIn , cb1], (req, res, next)=>{
+    cb2()
+    res.sendFile('profile.html')
 })
 
-app.get('/products', (req, res)=>{
-    console.log(req.query);
-    res.send('hiiiii')
-})
-
-app.listen(4000, ()=>{
-    console.log('server is running at port 4000')
+app.listen(8000, ()=>{
+    console.log('server is running at port 8000')
 })
--- SECTION-C/LEC-4/server.js
+++ SECTION-C/LEC-4/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const app = express();
-const isLoggedIn = require('./isLoggedIn.js')
 
-function cb1(req, res, next){
-    next()
-}
-function cb2(req, res, next){
-    
-}
-
-
-app.get('/profile',[isLoggedIn , cb1], (req, res, next)=>{
-    cb2()
-    res.sendFile('profile.html')
+app.get('/product/:id([0-9])/:price', (req, res)=>{
+    const {id, price} = req.params;
+    console.log(id)
+    console.log(price)
+    res.send('hello');
 })
 
-app.listen(8000, ()=>{
-    console.log('server is running at port 8000')
+app.get('/products', (req, res)=>{
+    console.log(req.query);
+    res.send('hiiiii')
+})
+
+app.listen(4000, ()=>{
+    console.log('server is running at port 4000')
 })
